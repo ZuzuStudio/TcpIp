8ccad0f8cb6d40c4a2b912ccff7e0e45cdfa7a2c
Harald Zealot <harald_zealot@tut.by>
2013-03-22 14:25:08 +0300

    simple server simple client

---
+++ simplec/main.c
@@ -0,0 +1,61 @@
+#include <sys/socket.h>
+#include <arpa/inet.h> //нужен для структуры sockaddr_in; а также для фунуций htons и inet_addr
+#include <stdio.h>
+
+
+int main ()
+{
+   int mySocket;
+
+   mySocket = socket (AF_INET, SOCK_STREAM, 0);
+
+   if (mySocket < 0)
+   {
+      printf ("ошибка вызова socket\n");
+      return 1;
+   }
+
+   int ok;
+   struct sockaddr_in myAddress;
+   myAddress.sin_family = AF_INET;
+   myAddress.sin_port = htons (7500);
+   myAddress.sin_addr.s_addr = inet_addr ("127.0.0.1");
+
+   ok = connect (mySocket, (struct sockaddr *) &myAddress, sizeof (myAddress));
+
+   if (ok)
+   {
+      printf ("ошибка вызова connect\n");
+      return 1;
+   }
+
+   //*
+   printf("local ip %s\n",inet_ntoa(myAddress.sin_addr));
+   printf("local port %d\n",htons(myAddress.sin_port));
+   printf("local domain(family) %s\n\n",(myAddress.sin_family==AF_INET?"AF_INET":"others"));
+   //*/
+
+   ok = send (mySocket, "kuku\0", 5, 0);
+
+   if (ok <= 0)
+   {
+      printf ("ошибка вызова send\n");
+      return 1;
+   }
+
+   char buf[ 5 ] ;
+
+   ok = recv (mySocket, buf, 5, 0) ;
+
+   if (ok <= 0)
+   {
+      printf ("ошибка вызова recv\n");
+      return 1;
+   }
+   else
+      printf ("%s\n", buf);
+
+   return 0;
+
+}
+
---
+++ simples/main.c
@@ -0,0 +1,94 @@
+#include <sys/socket.h>
+#include <arpa/inet.h>//тут працавала і з <netinet/in.h>, але для падабенства з кліентам падключыў больш шырокую арпу.
+#include <stdio.h>
+
+int main ()
+{
+   int mySocket;
+   mySocket = socket (AF_INET, SOCK_STREAM, 0);
+
+   if (mySocket < 0)
+   {
+      printf ("ошибка вызова socket\n");
+      return 1;
+   }
+
+   int bad;
+   struct sockaddr_in localAddress;
+   localAddress.sin_family = AF_INET;
+   localAddress.sin_port = htons (7500);
+   localAddress.sin_addr.s_addr = htonl (INADDR_ANY);
+   bad = bind (mySocket, (struct sockaddr *) &localAddress, sizeof (localAddress));
+
+   if (bad < 0)
+   {
+      printf ("ошибка вызова bind\n");
+      return 1;
+   }
+
+   bad = listen (mySocket, 5);
+
+   if (bad)
+   {
+      printf ("ошибка вызова listen\n");
+      return 1;
+   }
+
+   /*
+   int conectionSocket;
+   conectionSocket = accept (mySocket, NULL, NULL);
+
+   if (conectionSocket < 0)
+   {
+      printf ("ошибка вызова accept\n");
+      return 1;
+   }
+   //*/
+
+   ////////////////
+   // больш складаны варыянт, які атрымлівае адрас кліента
+   ///*
+   int conectionSocket;
+   struct sockaddr_in clientAddress;
+   int sizeClientAddress;
+
+   conectionSocket = accept (mySocket, (struct sockaddr *)&clientAddress, &sizeClientAddress);
+
+   if (conectionSocket < 0)
+   {
+      printf ("ошибка вызова accept\n");
+      return 1;
+   }
+   printf("local ip %s\n",inet_ntoa(localAddress.sin_addr));
+   printf("local port %d\n",htons(localAddress.sin_port));
+   printf("local domain(family) %s\n\n",(localAddress.sin_family==AF_INET?"AF_INET":"others"));
+   printf("client ip %s\n",inet_ntoa(clientAddress.sin_addr));
+   printf("client port %d\n",htons(clientAddress.sin_port));
+   printf("client domain(family) %s\n\n\n",(clientAddress.sin_family==AF_INET?"AF_INET":"others"));
+   //*/
+
+   char buf[5];
+   bad = recv (conectionSocket, buf, 5, 0);
+
+   if (bad <= 0)
+   {
+      printf ("ошибка вызова recv\n");
+      return 1;
+   }
+
+   printf ("%s\n", buf);
+   int i;
+
+   for (i = 0; i < 5; ++i)
+      scanf ("%c", &buf[i]);
+
+   bad = send (conectionSocket, buf, 5, 0);
+
+   if (bad <= 0)
+   {
+      printf ("ошибка вызова send\n");
+      return 1;
+   }
+
+   return 0;
+}
